da08484a108300686eb8d690bffc73bb6f9ee6cf
Wife-Haver <marcus12.dimapilis@gmail.com>
2024-11-28 15:15:30 +0800

    version hawk tuah 0.6

---
+++ tools.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import *
+
+class Tools:
+    def __init__(self,parent):
+        self.frame = tk.Frame(parent,bg="white")
+
+
+
+
+
+
+    def get_frame(self):
+        return self.frame
+
+
+
+
+
+
